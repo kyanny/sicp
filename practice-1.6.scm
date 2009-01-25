f984c0f3172fc4b6acb790085c651db627d2dfbd
Kensuke Kaneko <kyanny@gmail.com>
2009-01-25 23:48:27 +0900

    add practice 1.6 code.

---
+++ practice-1.6.scm
@@ -0,0 +1,30 @@
+;; 
+(define (new-if predicate then-clause else-clause)
+  (cond (predicate then-clause)
+        (else else-clause)))
+
+(new-if (= 2 3) 0 5)
+(new-if (= 1 1) 0 5)
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (square x)
+  (* x x))
+
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                     x)))
+
+(define (sqrt x)
+  (sqrt-iter x 1.0))
+
+(sqrt 3)
